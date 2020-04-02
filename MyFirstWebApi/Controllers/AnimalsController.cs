f28f321d04e79eae1ee98111d9799391c31e864e
Nathan Gonzalez <ncgonz@gmail.com>
2020-04-01 21:10:56 -0500

    get, get all and post

--- MyFirstWebApi/Controllers/AnimalsController.cs
+++ MyFirstWebApi/Controllers/AnimalsController.cs
@@ -12,16 +12,60 @@ namespace MyFirstWebApi.Controllers
     [ApiController]
     public class AnimalsController : ControllerBase
     {
+
+        static List<Animal> _animals = new List<Animal>
+                                    {
+                                        new Animal {Id = 1, Name = "Steve", Type = "Elephant", Weight = 2000},
+                                        new Animal {Id = 2, Name = "George", Type = "Monkey", Weight = 87},
+                                        new Animal {Id = 3, Name = "Tony", Type = "Tiger", Weight = 1200}
+                                    };
+
+        // GET /api/animals
         [HttpGet]
         public IActionResult GetAllAnimals()
         {
-            var animals = new List<Animal>
+            return Ok(_animals);
+        }
+
+        // GET /api/animals/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetSingleAnimal(int id)
+        {
+            var animalIWant = _animals.FirstOrDefault(a => a.Id == id);
+
+            if (animalIWant == null)
             {
-                new Animal {Name = "Steve", Type = "Elephant", Weight = 2000},
-                new Animal {Name = "George", Type = "Monkey", Weight = 87},
-                new Animal {Name = "Tony", Type = "Tiger", Weight = 1200}
-            };
-            return Ok(animals);
+                return NotFound($"Animal with the id {id} was not found.");
+            }
+
+            return Ok(animalIWant);
+        }
+
+        // POST /api/animals
+        [HttpPost]
+        public IActionResult AddAnAnimal(Animal animalToAdd)
+        {
+            _animals.Add(animalToAdd);
+
+            return Ok(_animals);
+        }
+
+        // PUT /api/animals/{id}
+        [HttpPut("{id}")]
+        public IActionResult UpdateAnAnimal(int id, Animal animal)
+        {
+            var animalToUpdate = _animals.FirstOrDefault(a => a.Id == id);
+
+            if (animalToUpdate == null)
+            {
+                return NotFound("Can't Update cause it doesn't exist");
+            }
+
+            animalToUpdate.Name = animal.Name;
+            animalToUpdate.Weight = animal.Weight;
+            animalToUpdate.Type = animal.Type;
+
+            return Ok(animalToUpdate);
         }
 
 
--- MyFirstWebApi/Models/Animal.cs
+++ MyFirstWebApi/Models/Animal.cs
@@ -7,6 +7,7 @@ namespace MyFirstWebApi.Models
 {
     public class Animal
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
         public int Weight { get; set; }
